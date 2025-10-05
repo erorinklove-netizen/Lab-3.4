d48330226e934d73bbf5a6f6e4c9f61949d17ec1
erorinklove-netizen <erorink.love@gmail.com>
2025-10-05 14:28:48 +0300

    Add files via upload

---
+++ Lab 3.4/Lab 3.4.cpp
@@ -0,0 +1,29 @@
+﻿// Lab 3.4.cpp 
+// < Якимів Наталія > 
+// Лабораторна робота № 3.4
+// Розгалуження, задане плоскою фігурою.
+// Варіант 1
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double x, y, R;
+    cout << "x = ";  if (!(cin >> x)) return 0;
+    cout << "y = ";  if (!(cin >> y)) return 0;
+    cout << "R = ";  if (!(cin >> R)) return 0;
+
+    if (R < 0) R = fabs(R); // робимо радіус додатнім
+
+    bool insideCircle = (x * x + y * y <= R * R);
+    // верхній сектор: y >= 0 та y >= |x|
+    // нижній сектор: y <= 0 та y <= -|x|
+    bool inSector = (y >= 0 && y >= fabs(x)) || (y <= 0 && y <= -fabs(x));
+
+    if (insideCircle && inSector)
+        cout << "yes\n";
+    else
+        cout << "no\n";
+
+    return 0;
+}
